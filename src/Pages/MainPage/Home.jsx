555732ab2704ce708d7d512aebc6beb194030ec1
Awesh Choudhary <aweshchoudhary7@gmail.com>
2023-02-04 22:11:01 +0530

    SEO optimize

--- src/Pages/MainPage/Home.jsx
+++ src/Pages/MainPage/Home.jsx
@@ -6,6 +6,15 @@ const Home = () => {
   return (
     <>
       <Helmet>
+        <link rel="canonical" href="https://stellaraesthetics.in/" />
+        <meta
+          name="description"
+          content="stellar aesthetics, facial cosmetics, clinical cosmetology, trichology, micropigmentation and microblading, hair restoration, facial aesthetics"
+        />
+        <meta
+          name="keywords"
+          content="stellar aesthetics, facial cosmetics, clinical cosmetology, trichology, tricology, micropigmentation and microblading, hair restoration, facial aesthetics"
+        />
         <title>Best Cosmetics Treatment In India</title>
       </Helmet>
       <HeroSection />
